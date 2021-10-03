37bc5c63d2b3354a0a73e8f4cca497373d2fba9c
Luana Stephany <ls.luanastephany@gmail.com>
2021-10-03 16:19:50 -0300

    add fontes usando react font loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 import React from "react";
+import GoogleFonts, { Font } from "react-google-font-loader";
 import "./styles/global.scss";
 
+const fonts: Font[] = [
+  {
+    font: "Caveat",
+    weights: [400],
+  },
+  {
+    font: "Caveat Brush",
+    weights: [400],
+  },
+];
+
 function App() {
-  return <div className="App">rick and morty</div>;
+  return (
+    <>
+      <GoogleFonts fonts={fonts} />
+      <h1>Rick</h1>
+    </>
+  );
 }
 
 export default App;
